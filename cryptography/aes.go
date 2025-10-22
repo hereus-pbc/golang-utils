2c29f51b987353653f2d210ec1324217bd2caea5
Çağan Mert İŞLEK <accounts@islekcaganmert.me>
2025-10-22 11:11:44 +0300

    kek

---
+++ cryptography/aes.go
@@ -0,0 +1,90 @@
+package password_encryption
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+
+	"golang.org/x/crypto/pbkdf2"
+)
+
+func pkcs7Pad(data []byte, blockSize int) []byte {
+	padding := blockSize - len(data)%blockSize
+	padText := make([]byte, padding)
+	for i := range padText {
+		padText[i] = byte(padding)
+	}
+	return append(data, padText...)
+}
+
+func pkcs7Unpad(data []byte) ([]byte, error) {
+	length := len(data)
+	if length == 0 {
+		return nil, fmt.Errorf("pkcs7Unpad: data is empty")
+	}
+	padding := int(data[length-1])
+	if padding == 0 || padding > length {
+		return nil, fmt.Errorf("pkcs7Unpad: invalid padding")
+	}
+	for i := 0; i < padding; i++ {
+		if data[length-1-i] != byte(padding) {
+			return nil, fmt.Errorf("pkcs7Unpad: invalid padding bytes")
+		}
+	}
+	return data[:length-padding], nil
+}
+
+func AesEncryptBytes(password, content []byte) (string, error) {
+	salt := make([]byte, 16)
+	_, err := io.ReadFull(rand.Reader, salt)
+	if err != nil {
+		return "", err
+	}
+	key := pbkdf2.Key(password, salt, 100000, 32, sha256.New)
+	iv := make([]byte, 16)
+	_, err = io.ReadFull(rand.Reader, iv)
+	if err != nil {
+		return "", err
+	}
+	padded := pkcs7Pad(content, aes.BlockSize)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	mode := cipher.NewCBCEncrypter(block, iv)
+	ciphertext := make([]byte, len(padded))
+	mode.CryptBlocks(ciphertext, padded)
+	return hex.EncodeToString(salt) + hex.EncodeToString(iv) + hex.EncodeToString(ciphertext), nil
+}
+
+func AesDecryptBytes(password []byte, encrypted string) ([]byte, error) {
+	salt, err := hex.DecodeString(encrypted[:32])
+	if err != nil {
+		return nil, err
+	}
+	iv, err := hex.DecodeString(encrypted[32:64])
+	if err != nil {
+		return nil, err
+	}
+	ciphertext, err := hex.DecodeString(encrypted[64:])
+	if err != nil {
+		return nil, err
+	}
+	key := pbkdf2.Key(password, salt, 100000, 32, sha256.New)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	mode := cipher.NewCBCDecrypter(block, iv)
+	padded := make([]byte, len(ciphertext))
+	mode.CryptBlocks(padded, ciphertext)
+	unpadded, err := pkcs7Unpad(padded)
+	if err != nil {
+		return nil, err
+	}
+	return unpadded, nil
+}
---
+++ cryptography/kek.go
@@ -0,0 +1,53 @@
+package password_encryption
+
+import (
+	"fmt"
+
+	"github.com/hereus-pbc/golang-utils/randomizer"
+)
+
+func GenerateKek(password []byte) (string, error) {
+	randomContent := randomizer.Random64Byte()
+	ret, err := AesEncryptBytes(password, randomContent)
+	if err != nil {
+		return "", err
+	}
+	return ret, nil
+}
+
+func EncryptWithKek(password string, kek string, content []byte) (string, error) {
+	l1, err := AesDecryptBytes([]byte(password), kek)
+	if err != nil {
+		return "", err
+	}
+	if len(l1) != 64 {
+		return "", fmt.Errorf("invalid layer 2 content length: expected 64 bytes, got %d", len(l1))
+	}
+	ret, err := AesEncryptBytes(l1, content)
+	if err != nil {
+		return "", err
+	}
+	return ret, nil
+}
+
+func DecryptWithKek(password string, kek string, encrypted string) ([]byte, error) {
+	l1, err := AesDecryptBytes([]byte(password), kek)
+	if err != nil {
+		return nil, err
+	}
+	if len(l1) != 64 {
+		return nil, fmt.Errorf("invalid layer 2 content length: expected 64 bytes, got %d", len(l1))
+	}
+	return AesDecryptBytes(l1, encrypted)
+}
+
+func DeriveKek(oldPassword string, kek string, newPassword string) (string, error) {
+	l1, err := AesDecryptBytes([]byte(oldPassword), kek)
+	if err != nil {
+		return "", err
+	}
+	if len(l1) != 64 {
+		return "", fmt.Errorf("invalid layer 2 content length: expected 64 bytes, got %d", len(l1))
+	}
+	return AesEncryptBytes([]byte(newPassword), l1)
+}
