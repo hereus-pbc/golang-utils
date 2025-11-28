ba9eefe1ed221a8abdb6c1a0d7e759e669dff828
Çağan Mert İŞLEK <hello@islekcaganmert.me>
2025-11-28 09:40:37 +0300

    Rename Base64Encode and Base64Decode functions

--- base64/main.go
+++ base64/main.go
@@ -1,6 +1,6 @@
 package base64
 
-func Base64Encode(data []byte) string {
+func Encode(data []byte) string {
 	var buf bytes.Buffer
 	enc := base64.NewEncoder(base64.StdEncoding, &buf)
 	_, err := enc.Write(data)
@@ -13,7 +13,7 @@ func Base64Encode(data []byte) string {
 	return buf.String()
 }
 
-func Base64Decode(data string) ([]byte, error) {
+func Decode(data string) ([]byte, error) {
 	decodedData, err := base64.StdEncoding.DecodeString(data)
 	if err != nil {
 		return nil, err
